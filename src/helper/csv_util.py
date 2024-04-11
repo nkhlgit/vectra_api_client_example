5c042bc77c4502e6ae0e9c1dc6a4296a372ea2d6
Nikhil Kumar Singh <nikhil.eltrx@gmail.com>
2024-04-11 23:56:26 +1000

    Delete src/helper/csv_util.py

--- src/helper/csv_util.py
+++
@@ -1,8 +0,0 @@
-def to_csv(ext, data_list):
-    if ext == 'groups': 
-        for data in data_list:
-            data['members'] =  ','.join(data['members'])
-        return data_list
-    else:
-        return data_list
-        
